2e521f12a73738139f90cd627552d5a71c70dfae
Aidas Patapavicius <aidas@bitlgx.com>
2020-07-28 17:23:52 +0300

    menu vuex module

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import restaurants from './modules/restaurants';
+import menu from './modules/menu';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     restaurants,
+    menu,
   },
 });
