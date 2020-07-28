d795c6c3c4a79e9fb1011014d690b2756a1267e5
Aidas Patapavicius <aidas@bitlgx.com>
2020-07-28 20:49:34 +0300

    item card select border transition

--- src/components/restaurant/menu/MenuItem.vue
+++ src/components/restaurant/menu/MenuItem.vue
@@ -1,23 +1,27 @@
 <template>
-  <d-card class="menu-item__card">
+  <d-card
+    @click="isSelected = !isSelected"
+    :class="['menu-item__card', isSelected && 'menu-item__card--selected']"
+  >
     <d-card-body class="menu-item__card-body">
       <d-row>
         <d-col :md="1">
           <span class="menu-item__plus">+</span>
         </d-col>
         <d-col>
-          <h4 class="menu-item__card-title">{{ title }}</h4>
-          <p class="text-muted">
-            {{ formatDescription }}
-            <br />
-            <span class="menu-item__card-price">€2.00</span>
-          </p>
+          <h4 class="menu-item__card-body-title">{{ title }}</h4>
+          <div class="menu-item__card-body-description__wrapper">
+            <p class="text-muted">
+              {{ description }}
+            </p>
+          </div>
+          <span class="menu-item__card-body-price">€2.00</span>
         </d-col>
         <d-col :md="5" style="padding-top: 20px;">
           <blur-hash-image
             width="600"
             height="337.5"
-            class="menu-item__card-image"
+            class="menu-item__card-body-image"
             :hash="image.blurhash"
             :src="image.url"
             alt="green lawn grass during daytime"
@@ -36,15 +40,10 @@ export default {
     description: String,
     image: Object,
   },
-  computed: {
-    formatDescription() {
-      return this.description.length > 80
-        ? `${this.description
-            .split('')
-            .splice(0, 80)
-            .join('')}...`
-        : this.description;
-    },
+  data() {
+    return {
+      isSelected: false,
+    };
   },
 };
 </script>
@@ -60,26 +59,38 @@ export default {
     border-radius: 0 !important;
     transition: 0.3s;
     border-top: 1px solid rgb(231, 231, 231);
-    &-title {
-      line-height: 15px;
-      font-size: 18px;
-      font-weight: 600;
-      color: #161616;
-      padding-top: 20px;
-    }
-    &-price {
-      color: #009de0;
-      margin-bottom: 0 !important;
-    }
-    &-image > span > img {
-      border-radius: 10px;
-    }
-    &:hover {
-      box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.23);
+    border-bottom: 1px solid rgb(231, 231, 231);
+    transition: 0.2s;
+    cursor: pointer;
+    &--selected {
+      border-left: 4px solid #009de0;
     }
     &-body {
       padding: 0 1rem;
       padding-bottom: 0.5rem;
+      &-title {
+        line-height: 15px;
+        font-size: 18px;
+        font-weight: 600;
+        color: #161616;
+        padding-top: 20px;
+      }
+      &-description {
+        &__wrapper {
+          height: 50px;
+          overflow: hidden;
+        }
+      }
+      &-price {
+        color: #009de0;
+        margin-bottom: 0 !important;
+      }
+      &-image > span > img {
+        border-radius: 10px;
+      }
+      &:hover {
+        box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.23);
+      }
     }
   }
 }
