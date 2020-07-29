84c8618f0ba38d60ab681b3adbdc71157830885c
Aidas Patapavicius <aidas@bitlgx.com>
2020-07-29 20:12:37 +0300

    search box

--- src/components/partials/Navbar/NavbarSearch.vue
+++ src/components/partials/Navbar/NavbarSearch.vue
@@ -1,25 +1,70 @@
 <template>
-  <d-input
-    :class="[transparent && 'navbar-transparent-search__input']"
-    size="sm"
-    placeholder="Search..."
-  />
+  <span>
+    <d-input
+      :class="[
+        'navbar-search__input',
+        transparent && 'navbar-search__input--transparent',
+      ]"
+      size="sm"
+      @input="handleSearchChange"
+      :value="query"
+      placeholder="Search..."
+    />
+    <div v-if="showSearchBox" class="navbar-search__container">
+      <d-card class="navbar-search__card">
+        <d-card-body>
+          <navbar-search-item />
+        </d-card-body>
+      </d-card>
+    </div>
+  </span>
 </template>
 <script>
+import NavbarSearchItem from './NavbarSearchItem.vue';
+
 export default {
   props: {
     transparent: Boolean,
   },
+  data() {
+    return {
+      showSearchBox: false,
+      query: '',
+    };
+  },
+  methods: {
+    handleSearchChange(value) {
+      if (value.length >= 3) {
+        this.showSearchBox = true;
+      }
+      if (value.length < 3) this.showSearchBox = false;
+      this.query = value;
+    },
+  },
+  components: {
+    NavbarSearchItem,
+  },
 };
 </script>
 <style lang="scss">
-.navbar-transparent-search {
+.navbar-search {
+  &__card {
+    position: absolute !important;
+    margin-top: 25px;
+    z-index: 999;
+  }
+  &__container {
+    width: 500px;
+  }
   &__input {
-    background-color: transparent !important;
-    border: 1.4px solid white !important;
-    color: white !important;
-    &::placeholder {
+    width: 500px !important;
+    &--transparent {
+      background-color: transparent !important;
+      border: 1.4px solid white !important;
       color: white !important;
+      &::placeholder {
+        color: white !important;
+      }
     }
   }
 }
---
+++ src/components/partials/Navbar/NavbarSearchItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <span>
+    <d-row style="width: 100%;">
+      <d-col :md="4">
+        <img
+          class="item__img"
+          src="http://localhost:8000/storage/restaurant_images/CPY1QqKt75xaakn3NlVuYIzIVqOuU17Z5iN0cSox.jpeg"
+          alt=""
+        />
+      </d-col>
+      <d-col>
+        <h5 class="item-content__title">Challenge</h5>
+        <p class="item-content__desc text-muted">Various cruisinces</p>
+        <p class="item-content__info text-muted mt-2">€€ - Vilniaus G - 8.4</p>
+      </d-col>
+    </d-row>
+  <hr>
+  </span>
+</template>
+<style lang="scss">
+.item {
+  &__img {
+    width: 110%;
+  }
+  &-content {
+    &__title {
+      font-weight: 800;
+    }
+    &__desc {
+      margin-bottom: 0 !important;
+      line-height: 0.2rem;
+    }
+    &__info {
+      margin-bottom: 0 !important;
+      color: rgb(226, 226, 226);
+      font-size: 13px;
+    }
+  }
+}
+</style>
